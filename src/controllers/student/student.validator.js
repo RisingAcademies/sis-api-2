4f9c536fe5951992b8b174dad7a9d556b4067490
Mihir Kanzariya <kanzariyamihir@gmail.com>
2020-08-26 16:24:38 +0530

    Pagination and document format

--- src/controllers/student/student.validator.js
+++ src/controllers/student/student.validator.js
@@ -1,33 +1,33 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const getStudentById = {
-  params: {
-    id: Joi.number().required(),
-  },
+	params: {
+		id: Joi.number().required(),
+	},
 };
 
 export const newValidation = {
-  body: {
-    userId: Joi.number().required(),
-  },
+	body: {
+		userId: Joi.number().required(),
+	},
 };
 
 export const addStudent = {
-  body: {
-    num: Joi.string().required(),
-    firstname: Joi.string().required(),
-    lastname: Joi.string().required(),
-    phone: Joi.number().required(),
-    address: Joi.string().required(),
-    age: Joi.number().required(),
-    mother: Joi.string().required(),
-    father: Joi.string().required(),
-  },
+	body: {
+		num: Joi.string().required(),
+		firstname: Joi.string().required(),
+		lastname: Joi.string().required(),
+		phone: Joi.number().required(),
+		address: Joi.string().required(),
+		age: Joi.number().required(),
+		mother: Joi.string().required(),
+		father: Joi.string().required(),
+	},
 };
 
 export const editStudent = {
-  body: {
-    id: Joi.number().required(),
-    attendanceId: Joi.number().required(),
-  },
+	body: {
+		id: Joi.number().required(),
+		attendanceId: Joi.number().required(),
+	},
 };
