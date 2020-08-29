550bc889e2402fc31cf779ffffc8b02e742ba417
Mihir Kanzariya <kanzariyamihir@gmail.com>
2020-08-29 22:13:39 +0530

    Error response code dynamic added

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,21 +1,22 @@
 export const successResponse = (req, res, data, code = 200) =>
-	res.send({
-		code,
-		data,
-		success: true,
-	});
+  res.send({
+    code,
+    data,
+    success: true,
+  });
 
 export const errorResponse = (
-	req,
-	res,
-	errorMessage = 'Something went wrong',
-	code = 500,
-	error = {},
-) =>
-	res.status(500).json({
-		code,
-		errorMessage,
-		error,
-		data: null,
-		success: false,
-	});
+  req,
+  res,
+  errorMessage = "Something went wrong",
+  code = 500,
+  error = {}
+) => {
+  return res.status(code).json({
+    code,
+    errorMessage,
+    error,
+    data: null,
+    success: false,
+  });
+};
