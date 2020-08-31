92adaae012b0cbb509a7f58ac7ccf9fd718f924d
Mihir Kanzariya <kanzariyamihir@gmail.com>
2020-08-31 23:40:38 +0530

    Feedback changes

--- src/migrations/20200821185456-create-attendances.js
+++ src/migrations/20200821185456-create-attendances.js
@@ -1,48 +1,49 @@
 module.exports = {
-  up: (queryInterface, Sequelize) =>
-    queryInterface.createTable("Attendances", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      studentId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Students",
-          key: "id",
-        },
-        onDelete: "restrict",
-        onUpdate: "CASCADE",
-      },
-      schoolId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Schools",
-          key: "id",
-        },
-        onDelete: "restrict",
-        onUpdate: "CASCADE",
-      },
-      grade: {
-        type: Sequelize.STRING,
-      },
-      percentage: {
-        type: Sequelize.INTEGER,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-      },
-      deletedAt: {
-        type: Sequelize.DATE,
-      },
-    }),
-  down: queryInterface => queryInterface.dropTable("Attendances"),
+	up: (queryInterface, Sequelize) =>
+		queryInterface.createTable('Attendances', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			studentId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: 'Students',
+					key: 'id',
+				},
+				onDelete: 'restrict',
+				onUpdate: 'CASCADE',
+			},
+			schoolId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: 'Schools',
+					key: 'id',
+				},
+				onDelete: 'restrict',
+				onUpdate: 'CASCADE',
+			},
+			grade: {
+				type: Sequelize.STRING,
+			},
+			percentage: {
+				type: Sequelize.INTEGER,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				type: Sequelize.DATE,
+			},
+			deletedAt: {
+				type: Sequelize.DATE,
+			},
+		}),
+	down: queryInterface =>
+		queryInterface.dropTable('Attendances'),
 };
