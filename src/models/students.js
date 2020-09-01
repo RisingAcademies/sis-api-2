ae2a132b05afa73ec7423597b3fc75374400f3d0
Mihir Kanzariya <kanzariyamihir@gmail.com>
2020-09-01 17:50:37 +0530

    Seeder createdAt issue resolved

--- src/models/students.js
+++ src/models/students.js
@@ -1,32 +1,35 @@
 module.exports = (sequelize, DataTypes) => {
-	const Students = sequelize.define(
-		'Students',
-		{
-			num: DataTypes.STRING,
-			firstname: DataTypes.STRING,
-			lastname: DataTypes.STRING,
-			phone: DataTypes.STRING,
-			address: DataTypes.STRING,
-			age: DataTypes.STRING,
-			mother: DataTypes.STRING,
-			father: DataTypes.STRING,
-			registeredDate: DataTypes.DATE,
-			deletedAt: DataTypes.DATE,
-			createdAt: DataTypes.DATE,
-			updatedAt: DataTypes.DATE,
-		},
-		{
-			paranoid: true,
-		},
-	);
-	Students.associate = (models) => {
-		// associations can be defined here
-		Students.belongsToMany(models.Schools, {
-			through: 'Attendances',
-			as: 'Schools',
-			foreignKey: 'studentId',
-			otherKey: 'schoolId',
-		});
-	};
-	return Students;
+  const Students = sequelize.define(
+    "Students",
+    {
+      num: DataTypes.STRING,
+      firstname: DataTypes.STRING,
+      lastname: DataTypes.STRING,
+      phone: DataTypes.STRING,
+      address: DataTypes.STRING,
+      age: DataTypes.STRING,
+      mother: DataTypes.STRING,
+      father: DataTypes.STRING,
+      registeredDate: DataTypes.DATE,
+      deletedAt: DataTypes.DATE,
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.literal("NOW()"),
+      },
+      updatedAt: DataTypes.DATE,
+    },
+    {
+      paranoid: true,
+    }
+  );
+  Students.associate = (models) => {
+    // associations can be defined here
+    Students.belongsToMany(models.Schools, {
+      through: "Attendances",
+      as: "Schools",
+      foreignKey: "studentId",
+      otherKey: "schoolId",
+    });
+  };
+  return Students;
 };
