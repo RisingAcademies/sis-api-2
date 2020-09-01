3e3daa88e155370d56e405f1a36cb951827c630d
Mihir Kanzariya <kanzariyamihir@gmail.com>
2020-09-01 19:17:26 +0530

    Feedback changes

--- src/migrations/20200821173711-create-schools.js
+++ src/migrations/20200821173711-create-schools.js
@@ -1,29 +1,30 @@
 module.exports = {
-  up: (queryInterface, Sequelize) =>
-    queryInterface.createTable("Schools", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      name: {
-        type: Sequelize.STRING,
-      },
-      country: {
-        type: Sequelize.STRING,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("NOW()"),
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-      },
-      deletedAt: {
-        type: Sequelize.DATE,
-      },
-    }),
-  down: (queryInterface) => queryInterface.dropTable("Schools"),
+	up: (queryInterface, Sequelize) =>
+		queryInterface.createTable('Schools', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			name: {
+				type: Sequelize.STRING,
+			},
+			country: {
+				type: Sequelize.STRING,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal('NOW()'),
+			},
+			updatedAt: {
+				type: Sequelize.DATE,
+			},
+			deletedAt: {
+				type: Sequelize.DATE,
+			},
+		}),
+	down: queryInterface =>
+		queryInterface.dropTable('Schools'),
 };
--- src/models/schools.js
+++ src/models/schools.js
@@ -1,28 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-  const Schools = sequelize.define(
-    "Schools",
-    {
-      name: DataTypes.STRING,
-      country: DataTypes.STRING,
-      deletedAt: DataTypes.DATE,
-      createdAt: {
-        type: DataTypes.DATE,
-        defaultValue: sequelize.literal("NOW()"),
-      },
-      updatedAt: DataTypes.DATE,
-    },
-    {
-      paranoid: true,
-    }
-  );
-  Schools.associate = (models) => {
-    Schools.belongsToMany(models.Students, {
-      through: "Attendances",
-      as: "Students",
-      foreignKey: "schoolId",
-      otherKey: "studentId",
-    });
-    // associations can be defined here
-  };
-  return Schools;
+	const Schools = sequelize.define(
+		'Schools',
+		{
+			name: DataTypes.STRING,
+			country: DataTypes.STRING,
+			deletedAt: DataTypes.DATE,
+			createdAt: {
+				type: DataTypes.DATE,
+				defaultValue: sequelize.literal('NOW()'),
+			},
+			updatedAt: DataTypes.DATE,
+		},
+		{
+			paranoid: true,
+		},
+	);
+	Schools.associate = (models) => {
+		Schools.belongsToMany(models.Students, {
+			through: 'Attendances',
+			as: 'Students',
+			foreignKey: 'schoolId',
+			otherKey: 'studentId',
+		});
+		// associations can be defined here
+	};
+	return Schools;
 };
