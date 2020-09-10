a9137db968e1dd597f3d922c6aa98af91dfabd88
Mihir Kanzariya <kanzariyamihir@gmail.com>
2020-09-10 12:31:44 -0400

    Seeder changes

--- src/migrations/20200910142448-create-student-records.js
+++ src/migrations/20200910142448-create-student-records.js
@@ -1,59 +1,59 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("StudentRecords", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      studentId: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        references: {
-          model: "Students",
-          key: "uid",
-        },
-        onDelete: "restrict",
-        onUpdate: "CASCADE",
-      },
-      termId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Terms",
-          key: "id",
-        },
-        onDelete: "restrict",
-        onUpdate: "CASCADE",
-      },
-      grade: {
-        type: Sequelize.STRING(25),
-      },
-      schoolId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Schools",
-          key: "id",
-        },
-        onDelete: "restrict",
-        onUpdate: "CASCADE",
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      deletedAt: {
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  down: async (queryInterface) => {
-    await queryInterface.dropTable("StudentRecords");
-  },
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('StudentRecords', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			studentId: {
+				type: Sequelize.UUID,
+				allowNull: false,
+				references: {
+					model: 'Students',
+					key: 'uid',
+				},
+				onDelete: 'restrict',
+				onUpdate: 'CASCADE',
+			},
+			termId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: 'Terms',
+					key: 'id',
+				},
+				onDelete: 'restrict',
+				onUpdate: 'CASCADE',
+			},
+			grade: {
+				type: Sequelize.STRING(25),
+			},
+			schoolId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: 'Schools',
+					key: 'id',
+				},
+				onDelete: 'restrict',
+				onUpdate: 'CASCADE',
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			deletedAt: {
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface) => {
+		await queryInterface.dropTable('StudentRecords');
+	},
 };
