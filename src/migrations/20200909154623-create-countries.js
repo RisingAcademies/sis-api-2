31a276f162db102442461c6a7095510f9afdeb93
Mihir kanzariya <kanzariyamihir@gmail.com>
2020-09-22 15:41:12 +0530

    Removed logs and new users are added

--- src/migrations/20200909154623-create-countries.js
+++ src/migrations/20200909154623-create-countries.js
@@ -1,38 +1,39 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface
-      .createTable("Countries", {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        name: {
-          allowNull: false,
-          type: Sequelize.STRING(50),
-        },
-        code: {
-          allowNull: false,
-          type: Sequelize.STRING(50),
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.literal("NOW()"),
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.literal("NOW()"),
-        },
-        deletedAt: {
-          type: Sequelize.DATE,
-        },
-      })
-      .then(() => queryInterface.addIndex("Countries", ["name"]));
-  },
-  down: async (queryInterface) => {
-    await queryInterface.dropTable("Countries");
-  },
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface
+			.createTable('Countries', {
+				id: {
+					allowNull: false,
+					autoIncrement: true,
+					primaryKey: true,
+					type: Sequelize.INTEGER,
+				},
+				name: {
+					allowNull: false,
+					type: Sequelize.STRING(50),
+				},
+				code: {
+					allowNull: false,
+					type: Sequelize.STRING(50),
+				},
+				createdAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal('NOW()'),
+				},
+				updatedAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal('NOW()'),
+				},
+				deletedAt: {
+					type: Sequelize.DATE,
+				},
+			})
+			.then(() =>
+				queryInterface.addIndex('Countries', ['name']));
+	},
+	down: async (queryInterface) => {
+		await queryInterface.dropTable('Countries');
+	},
 };
